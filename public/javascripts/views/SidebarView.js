888ad4356701cc824c29f40adaef6f77bf6e978d
David Zhu <huanzhendavidzhu@gmail.com>
2015-04-04 22:19:30 -0400

    Colors added as a mixin.

--- public/javascripts/views/SidebarView.js
+++ public/javascripts/views/SidebarView.js
@@ -14,7 +14,25 @@ window.SidebarView = Backbone.View.extend({
     },
 
     onClick: function (ev){
-      console.log(ev.currentTarget.id);
+      // console.log(ev.currentTarget.id);
+      switch(ev.currentTarget.id) {
+        case "homeButton":
+          Backbone.history.loadUrl('#home');
+          break;
+        case "accountButton":
+          console.log("accountButton");
+          break;
+        case "freeButton":
+          console.log("freeButton");
+          break;
+        case "notificationsButton":
+          console.log("notificationsButton");
+          break;
+        case "logoutButton":
+          console.log("logoutButton");
+          break;
+        default:
+          return;
+      }
     },
-
 });

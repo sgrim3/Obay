7f7e117836e98f284dba4c5ada151fbc9cef650e
David Zhu <huanzhendavidzhu@gmail.com>
2015-04-04 22:00:11 -0400

    Profile button added.

--- public/javascripts/views/SidebarView.js
+++ public/javascripts/views/SidebarView.js
@@ -1,5 +1,9 @@
 window.SidebarView = Backbone.View.extend({
 
+    events: {
+      'click .round-button': 'onClick',
+    },
+
     initialize:function () {
         this.render();
     },
@@ -7,6 +11,10 @@ window.SidebarView = Backbone.View.extend({
     render:function () {
         this.$el.html(this.template());
         return this;
-    }
+    },
+
+    onClick: function (ev){
+      console.log(ev.currentTarget.id);
+    },
 
 });

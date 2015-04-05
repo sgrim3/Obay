c33e9c53aea31cf01210fbfac368159a703ca508
Nitya Dhanushkodi <nitya.dhanushkodi@students.olin.edu>
2015-04-05 19:57:49 -0400

    trying async

--- public/javascripts/views/ItemView.js
+++ public/javascripts/views/ItemView.js
@@ -1,7 +1,7 @@
 window.ItemView = Backbone.View.extend({
 
 	events: {
-	    'click .toggle-feeds-button': 'buyItem',
+	    'click .buyButton': 'buyItem',
 	},
 
 	initialize:function (options) {
@@ -10,33 +10,52 @@ window.ItemView = Backbone.View.extend({
 
     render: function(options){
     	console.log(options.id);
-    	// $el=options.el;
-    	if(options.id){
 
+    	//if there's an item id in the url, gets the item model
+    	//and makes an api call
+    	if(options.id){
     		this.item = new Listing({id:options.id});
     		console.log(this.item);
-
-    		var itemViewScope = this;
+			var itemViewScope = this; //BECAUSE ASYNC! DO THIS ALWAYS!
 
 	    	this.item.fetch({
-	    		//data: $.param(), 
 	    		success: function(item){
 	    			console.log(item.attributes);
 	    			itemViewScope.$el.html(itemViewScope.template(item.attributes));
 	         		return itemViewScope;
 	    		},
+
 	    		error: function(){
 	    			console.log('error!');
 	    		}
-
     	});
 	    }
 	    else{
-	    	console.log('no object to look for');
+	    	console.log('This item does not exist');
 	    }
     },
 
     buyItem: function(options){
-    	
+    	if(options.id){
+    		this.item = new Listing({id:options.id});
+    		var itemViewScope = this;
+
+    		this.item.set({
+    			item_open: false
+    			success: function(){
+    				console.log(this.item);
+    			}
+    			error: function(){
+    				console.log('this isnt async breh')
+    			}
+		    	
+    		});
+
+    		// this.item.save({
+
+    		// });
+
+    	}
     }
+
 });

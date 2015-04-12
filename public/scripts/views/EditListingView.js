f4eb74f0921f21640d9b0eb08fcc56fa0fb4d1b5
Nitya Dhanushkodi <nitya.dhanushkodi@students.olin.edu>
2015-04-12 18:49:15 -0400

    bug fix add listing template populated

--- public/scripts/views/EditListingView.js
+++ public/scripts/views/EditListingView.js
@@ -24,42 +24,9 @@ define([
             this.template = _.template(AddListingTemplate);
         },
 
-        // destroy:function () {
-        //     //destroy dropzone instance to prevent memory leaks
-        //     Dropzone.instances = _.without(Dropzone.instances, this.image_upload);
-        //     //destroys view and corresponding mount point /$el
-        //     this.childViews.forEach( function (childView) {
-        //         //destroy all child views!
-        //         childView.destroy();
-        //     });
-        //     // COMPLETELY UNBIND THE VIEW
-        //     this.undelegateEvents();
-        //     this.$el.removeData().unbind(); 
-        //     // Remove view from DOM
-        //     this.remove();  
-        //     Backbone.View.prototype.remove.call(this);
-        //     return this;
-        // },
 
         render:function (info) {
-            // console.log(info.model.id);
-            // var id = info.model.id;
-            // console.log($("#addListingName"));
-            //debugger;
-            // $("#addListingName").val("hi");
-            // console.log($("#addListingName").val());
 
-            // $(function(){
-            //     $("#addListingName").val(id);
-            // });
-            
-            // var listing_name = $("#addListingName").val();
-            // var listing_description = $("#addListingDescription").val();
-            // var listing_image = $("#addListingImage").val();
-            // var listing_price= $("#addListingPrice").val();            
-            // console.log(this.model.attributes);
-            // self_model = new Listing({id:info.model.id});
-            // console.log(self_model);
             var self = this;
             this.model.fetch({
                 success: function(listing){
@@ -119,17 +86,6 @@ define([
             });   
 
             
-
-            // var new_listing = new Listing(
-            //     {
-            //         //listing_creator and listing_time_created is set on the server
-            //         listing_name: listing_name,
-            //         listing_description: listing_description,
-            //         listing_image: listing_image,
-            //         listing_open: true,
-            //         listing_price: listing_price
-            //     }
-            // );
 
             //this save function looks funny because it's not a mongoose save, it's a backbone models .save!
             this.model.save({}, {

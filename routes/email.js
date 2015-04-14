80d70f337807b0d1304962e937101024ea998164
Allison Patterson <allison.patterson@students.olin.edu>
2015-04-13 23:13:38 -0400

    skeleton cashEmail message, important: current variables do not contain the appropriate information, they only indicate what information is intended to be there in the future

--- routes/email.js
+++ routes/email.js
@@ -34,6 +34,18 @@ var email = {
 
 
     email.sendEmail(res, mailOptions);
+  },
+    cashEmail: function (req, res, next){
+    // Setup e-mail data with unicode symbols.
+    var mailOptions = {
+      from: 'Olin Obay<noreply@obay.herokuapp.com>', // sender address
+      to: req.body.emailTo ||'allisongpatterson@gmail.com', // list of receivers
+      subject: req.body.emailSubject || 'Obay Reciept for ' + item_name // Subject line
+      text: req.body.emailText || 'This is an aknowledgement that ' + buyer + ' has successfully purchased ' + item_name + ' from ' + seller + '. By making this purchase, ' + buyer + ' has agreed to pay the amount of ' + item_price + ' dollars to ' + seller + '. Please save this message for your records until the IOU is resolved.' // plaintext body
+    };
+
+
+    email.sendEmail(res, mailOptions);
   }
 };
 
